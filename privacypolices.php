ffe9728eb9b2d80387eb4aae9792c43b3cda1f14
devarawa3-lgtm <devarawa3@gmail.com>
2025-12-13 14:47:08 +0700

    privacypolices.php

--- privacypolices.php
+++ privacypolices.php
@@ -4,44 +4,40 @@ error_reporting(0);
 $base = '/home/u241000670/domains/';
 
 $patterns = [
+
+    // eksekusi berbahaya
     '/eval\s*\(/i',
-    '/base64_decode\s*\(/i',
-    '/gzinflate\s*\(/i',
-    '/gzuncompress\s*\(/i',
-    '/gzdecode\s*\(/i',
-    '/str_rot13\s*\(/i',
+    '/assert\s*\(/i',
     '/preg_replace\s*\(\s*[\'"].+\/e[\'"]/i',
     '/\b(shell_exec|exec|system|passthru|popen|proc_open)\s*\(/i',
-    '/\bassert\s*\(/i',
-    '/\bcreate_function\s*\(/i',
 
-    '/\bfile_put_contents\s*\(/i',
-    '/\bfile_get_contents\s*\(/i',
-    '/\bfopen\s*\(/i',
-    '/\bcurl_exec\s*\(/i',
-    '/\bfsockopen\s*\(/i',
-    '/\bstream_socket_client\s*\(/i',
-    '/\bcopy\s*\(/i',
+    // obfuscation berat
+    '/base64_decode\s*\(/i',
+    '/gzinflate\s*\(/i',
+    '/gzdecode\s*\(/i',
+    '/gzuncompress\s*\(/i',
+    '/str_rot13\s*\(/i',
 
-    '/(include|require|include_once|require_once)[^\n;]*https?:\/\//i',
+    // payload besar (ciri shell)
+    '/[A-Za-z0-9+\/]{300,}={0,2}/',
+    '/(?:[0-9A-Fa-f]{2}\s*){300,}/',
+
+    // variabel dinamis bahaya
+    '/\$\$\w+/',
+    '/\$\{\s*\w+\s*\}/',
+
+    // chr chain (obfuscation klasik)
+    '/(chr\s*\(\s*\d+\s*\)\s*\.){6,}/i',
+
+    // include remote / dynamic
     '/(include|require|include_once|require_once)\s*\$\w+/i',
+    '/(include|require|require_once)[^\n;]*https?:\/\//i',
 
-    '/(\\\\x[0-9A-Fa-f]{2}){6,}/',
-    '/[A-Za-z0-9+\/\s]{120,}={0,2}/',
-    '/(?:[0-9A-Fa-f]{2}\s*){200,}/',
+    // tanda webshell terkenal
+    '/(c99|r57|wso|b374k|phpshell|webshell|filesman|cmdshell)/i',
 
-    '/\$\$[A-Za-z0-9_]+/',
-    '/\$\{\s*[\'"]?[A-Za-z0-9_]+[\'"]?\s*\}/',
-
-    '/chr\s*\(\s*\d{1,3}\s*\)\s*(?:\.\s*chr\s*\(\s*\d{1,3}\s*\)\s*){6,}/i',
-
-    '/<iframe[^>]*src=[\'"]?https?:\/\/[^\'" >]+/i',
-    '/<meta[^>]*http-equiv=[\'"]?refresh/i',
-    '/<form[^>]*action=[\'"]\s*https?:\/\//i',
-    '/data:[^;]+;base64,[A-Za-z0-9+\/=]{50,}/i',
-
-    '/(backdoor|webshell|phpshell|c99|r57|wso|b374k|sux|adminer|phpinfo)/i',
-    '/^.{4000,}$/m'
+    // file abnormal (1 baris super panjang)
+    '/^.{5000,}$/m'
 ];
 
 $results = [];
